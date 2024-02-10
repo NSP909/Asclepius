fe3c191bce18fca89ce3dace03f992bd7d8e6619
Aryan Tajne <atajne@umd.edu>
2024-02-10 05:44:05 -0500

    e

--- parse_query.py
+++ parse_query.py
@@ -16,4 +16,14 @@ You are a natural language processor, that converts natural language to a struct
 """)
 
 def parse_query(input_text):
-    pass
+    dicty = {
+        "notes": ["note", "note_date"],
+        "medicine": ["med_name", "med_dosage", "med_frequency", "med_date"],
+        "vaccine": ["vac_name", "vac_date"],
+        "lab_result": ["lab_result", "lab_date"],
+        "surgeries": ["surgery", "surgery_date"],
+        "emergencies": ["emergency_name", "emergency_date"],
+        "vitals": ["vital_name", "vital_value", "vital_date"],
+        "diagnosis": ["diagnosis", "diag_date"],
+        "symptoms": ["symptom", "symptom_date"]
+    }    
