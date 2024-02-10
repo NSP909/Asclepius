c84504e0e252643c2af406815551c97c09483915
Aryan Tajne <atajne@umd.edu>
2024-02-10 17:46:56 -0500

    changed name

--- flask_server.py
+++ flask_server.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect
-from flask import request
-from flask import jsonify
+from flask_server import Flask, redirect
+from flask_server import request
+from flask_server import jsonify
 import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
