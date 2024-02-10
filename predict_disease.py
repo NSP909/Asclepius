1579b436cbce8a3db84622c9b8b357ab3805a63e
Dahong Luo <dappon.04@gmail.com>
2024-02-10 18:48:53 -0500

    implemented disease probaility

--- predict_disease.py
+++ predict_disease.py
@@ -2,7 +2,6 @@ from gemini_context_manager import GeminiContextManager
 from dotenv import load_dotenv
 import google.generativeai as genai
 import os
-from flask import get_all_data
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
@@ -29,7 +28,7 @@ safety_settings = [
         
 model = genai.GenerativeModel("gemini-pro",safety_settings=safety_settings)
 
-message = """
+PROMPT = """
 You are a neural network model that predicts diseases and its probability based on the medical record provided below.
 Provide 3 diseases and their probability in the following format, in decending order of probability.
 
@@ -40,18 +39,17 @@ return format:
 {{ [{{"disease": "disease_name", "probability": "~probability~"}}, ...]}}
 
 IMPORTANT: FOLLOW THE PROVIDED RETURN FORMAT EXACTLY.
+IMPORTANT: USE THE ACTUAL DISEASE NAME.
 """
 
-def predict_disease(user_id):
-    if type(user_id) != str:
-        user_id = str(user_id)
-    info_list = get_all_data(user_id)
+def predict_disease(input_data=None):
     
-    """chat = model.start_chat()
-    response = chat.send_message()"""
+    chat = model.start_chat()
+    response = chat.send_message(PROMPT.format(input=str(input_data)))
+    print(response.text)
+    return response.text
 
 def main():
-    name = "1"
     predict_disease()
 
 if __name__ == "__main__":
