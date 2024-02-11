b49470f9c83a8a502a6f54709e15f0e9eb567714
Swastik Agrawal <swastikagrawal@Swastiks-MacBook-Air.local>
2024-02-10 21:54:40 -0500

    some frontend changes

--- frontend/src/pages/DoctorView.js
+++ frontend/src/pages/DoctorView.js
@@ -9,7 +9,7 @@ const Main = () => {
   return (
     <div className="flex w-full h-screen overflow-y-hidden">
       <Sidebar />
-      {/* this is the upload file page */}
+      
       <Routes>
         <Route path="/add" element={<AddReport />} />
         <Route path="/" element={<DashBoard />} />
@@ -25,7 +25,7 @@ const Sidebar = () => {
       <div className="p-3">
         <a
           href="#"
-          className="text-white text-3xl font-semibold uppercase hover:text-gray-300"
+          className="text-white text-3xl font-semibold hover:text-gray-300"
         >
           DuoDoc
         </a>
@@ -40,8 +40,8 @@ const Sidebar = () => {
           href="#"
           className="flex items-center bg-active_nav_link text-white py-4 pl-6 nav-item"
         >
-          <i className="fas fa-tachometer-alt mr-3"></i>
-          Doctor's Dashboard
+          <i className="fas fa-user-friends mr-3"></i>
+          Patient List
         </a>
         <Link to="/d/reports">
           <button className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
@@ -53,28 +53,16 @@ const Sidebar = () => {
           className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item"
         >
           <i className="fas fa-table mr-3"></i>
-          Predictions
+          Search & Predict
         </a>
         <a
           href="forms.html"
           className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item"
         >
           <i className="fas fa-align-left mr-3"></i>
-          Account Settings
+           Settings
         </a>
-        {/* <a href="tabs.html" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-          <i className="fas fa-tablet-alt mr-3"></i>
-          Tabbed Content
-        </a>
-        <a href="calendar.html" className="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
-          <i className="fas fa-calendar mr-3"></i>
-          Calendar
-        </a> */}
       </nav>
-      {/* <a href="#" className="absolute w-full upgrade-btn bottom-0 active-nav-link text-white flex items-center justify-center py-4">
-        <i className="fas fa-arrow-circle-up mr-3"></i>
-        Upgrade to Pro!
-      </a> */}
     </aside>
   );
 };
