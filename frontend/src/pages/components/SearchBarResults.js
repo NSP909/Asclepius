ef2d407a17577eed74d37bafff8f7d2c5f3d07b5
Loldude0 <atajne@umd.edu>
2024-02-11 10:09:18 +0000

    "tailwind is sus"

--- frontend/src/pages/components/SearchBarResults.js
+++ frontend/src/pages/components/SearchBarResults.js
@@ -37,26 +37,30 @@ const SearchPredict = () => {
   const headers = results.length > 0 ? Object.keys(results[0]) : [];
 
   return (
-    <div className="flex flex-col items-center gap-10">
-      <form onSubmit={handleSubmit} className="flex items-center border rounded-md shadow-md overflow-hidden">
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={handleChange}
-          placeholder="Search..."
-          className="py-2 px-4 w-[30vw] outline-none h-[8vh]"
-        />
-        <button type="submit" className="bg-headerColor hover:bg-blue-600 text-white py-2 px-4 h-[8vh]">
-          Search
-        </button>
-      </form>
-      {sql && <div className=" bg-gray-200 h-[8vh] max-h-[16vh] w-[60vw] overflow-y-auto border-dashed border-2 border-gray-900 p-4">
-        <code>{sql}</code>
-      </div>}
-      {sql && <button type="submit" onClick={onSubmit} className="bg-headerColor hover:bg-gray-600 text-white py-2 px-4 rounded-full">
-        <i class="fas fa-check"></i>
-      </button>}
-      <div style={{ 
+  <div className="flex flex-col items-center gap-10">
+    <form onSubmit={handleSubmit} className="flex items-center border rounded-md shadow-md overflow-hidden">
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={handleChange}
+        placeholder="Search..."
+        className="py-2 px-4 w-[30vw] outline-none h-[8vh]"
+      />
+      <button type="submit" className="bg-headerColor hover:bg-blue-600 text-white py-2 px-4 h-[8vh]">
+        Search
+      </button>
+    </form>
+    {sql && <div className=" bg-gray-200 h-[8vh] max-h-[16vh] w-[60vw] overflow-y-auto border-dashed border-2 border-gray-900 p-4">
+      <code>{sql}</code>
+    </div>}
+    {sql && <button type="submit" onClick={onSubmit} className="bg-headerColor hover:bg-gray-600 text-white py-2 px-4 rounded-full">
+      <i class="fas fa-check"></i>
+    </button>}
+    <div style={{ 
+      maxHeight: '300px', // adjust as needed
+      overflowY: 'auto'
+    }}>
+      <div className = '' style={{ 
         display: 'table', 
         boxShadow: '0px 0px 8px 2px rgba(0,0,0,0.1)', 
         borderRadius: '10px', 
@@ -116,7 +120,8 @@ const SearchPredict = () => {
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default SearchPredict;
